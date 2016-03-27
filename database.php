8ab09fb131e7127f741dac7235eee386905a1f9f
Kelly Stathis <kstathis@wustl.edu>
2016-03-27 15:11:23 -0500

    add database.php to connect to mysql database

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+	// Connect to MySQL database news via php user
+	$mysqli = new mysqli('localhost', 'php', 'phpuserpass', 'payback_app');
+	 
+	if($mysqli->connect_errno) {
+		printf("Connection Failed: %s\n", $mysqli->connect_error);
+		exit;
+	} else {
+		printf("Connected to database!");
+	}
+?>
