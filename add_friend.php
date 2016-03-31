c3d3a581671bb4edaa255b8c68bdbe31edc0c988
Kelly Stathis <kstathis@wustl.edu>
2016-03-31 16:30:04 -0500

    Improvements to add friend - check email validity before seeing if user already exists, and check to make sure user isn't adding themself as a friend

--- add_friend.php
+++ add_friend.php
@@ -17,54 +17,60 @@
     $stmt->fetch();
     $stmt->close();
     
+    // FIXME: check if user is already friends with the user
     
+    // Check if email valid
+    if(!preg_match('/\S+@\S+\.\S+/', $email) ){
+        echo json_encode(array(
+            "success" => false,
+            "message" => "Invalid email address."
+        ));
+        exit;
+    }
     // Check if user does not exist
-    if($cnt == 0){
+    else if ($cnt == 0){
         echo json_encode(array(
             "success" => false,
             "message" => "Friend does not exist: have them make an account!"
         ));
         exit;
-    }  
-    else {
-        // Check email format
-        if(!preg_match('/\S+@\S+\.\S+/', $email) ){
-            echo json_encode(array(
-                "success" => false,
-                "message" => "Invalid email address."
-            ));
+    }
+    else if ($_SESSION['user_id'] == $friend_id) {
+        echo json_encode(array(
+            "success" => false,
+            "message" => "Cannot add yourself as a friend!"
+        ));
+        exit;
+    }
+    else { // Friend user exists, add to friend lists
+        // Use a prepared statement
+        $stmt = $mysqli->prepare("insert into friends (user_id, friend_id) values (?, ?)");
+        if(!$stmt){
+            printf("Query Prep Failed: %s\n", $mysqli->error);
             exit;
         }
-        else { // Friend user exists, add to friend lists
-            // Use a prepared statement
-            $stmt = $mysqli->prepare("insert into friends (user_id, friend_id) values (?, ?)");
-            if(!$stmt){
-                printf("Query Prep Failed: %s\n", $mysqli->error);
-                exit;
-            }
-            // Bind the parameter
-            $stmt->bind_param('ii', $user_id, $friend_id);
-            $user_id = $_SESSION['user_id'];
-            // Execute and close
-            $stmt->execute();
-            $stmt->close();
-            
-            // Use a prepared statement
-            $stmt2 = $mysqli->prepare("insert into friends (user_id, friend_id) values (?, ?)");
-            if(!$stmt2){
-                printf("Query Prep Failed: %s\n", $mysqli->error);
-                exit;
-            }
-            // Bind the parameter
-            $stmt2->bind_param('ii', $friend_id, $user_id);
-            // Execute and close
-            $stmt2->execute();
-            $stmt2->close();
-            
-            echo json_encode(array(
-                "success" => true
-            ));
+        // Bind the parameter
+        $stmt->bind_param('ii', $user_id, $friend_id);
+        $user_id = $_SESSION['user_id'];
+        // Execute and close
+        $stmt->execute();
+        $stmt->close();
+        
+        // Use a prepared statement
+        $stmt2 = $mysqli->prepare("insert into friends (user_id, friend_id) values (?, ?)");
+        if(!$stmt2){
+            printf("Query Prep Failed: %s\n", $mysqli->error);
             exit;
         }
+        // Bind the parameter
+        $stmt2->bind_param('ii', $friend_id, $user_id);
+        // Execute and close
+        $stmt2->execute();
+        $stmt2->close();
+        
+        echo json_encode(array(
+            "success" => true
+        ));
+        exit;
     }
 ?>
